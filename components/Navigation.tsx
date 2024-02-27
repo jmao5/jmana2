ad103eef923eb92999c3e915013183cd92ebe5aa
jmao5 <jsh5890@kakao.com>
2024-02-27 17:28:34 +0900

    모바일 네비게이션 수정

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,26 +1,26 @@
-import { Search } from "lucide-react";
+"use client";
+
 import Link from "next/link";
-import { Button } from "./ui/button";
+import { Search } from "lucide-react";
+import { useState } from "react";
 
 export default function Navigation() {
   const buttonLabels = [
-    { label: "웹툰", key: 0, url: "/webton" },
+    { label: "웹툰", key: 0, url: "/webtoon" },
     { label: "만화", key: 1, url: "/manhua" },
     { label: "툰찾", key: 2, url: "/mark" },
   ];
 
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <header className="px-4 md:px-6 lg:px-8 xl:px-10 2xl:px-12 border-gray-300">
-      <nav className="flex justify-between items-center h-20">
+      <nav className="flex justify-between items-center h-12 md:h-20">
         <div className="flex items-center">
           <Link href={"/"}>로고</Link>
-          <div className="pl-10 mt-2 md:mt-0 space-x-2">
+          <div className="pl-10 mt-2 md:mt-0 space-x-2 hidden md:flex">
             {buttonLabels.map((button, index) => (
-              <Link
-                key={index}
-                href={button.url ?? "#"}
-                className="inline-block"
-              >
+              <Link key={index} href={button.url ?? "#"}>
                 <button className="text-black hover:bg-slate-100 border-none py-1 px-2 rounded-sm">
                   {button.label}
                 </button>
@@ -30,17 +30,27 @@ export default function Navigation() {
         </div>
         <div className="flex items-center">
           <div className="flex items-center mt-2 md:mt-0 pr-3">
-            <div className="flex items-center justify-center">
-              <Button variant="outlineNone" size="icon">
-                <Search />
-              </Button>
-              <input
-                type="text"
-                placeholder="제목을 검색해보세요"
-                className=" placeholder-black placeholder-opacity-75 py-2 pl-2"
-              />
-            </div>
+            <Search />
+            <input
+              type="text"
+              placeholder="제목을 검색해보세요"
+              className="placeholder-black placeholder-opacity-75 py-2 pl-2"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
           </div>
+        </div>
+      </nav>
+      {/* Mobile navigation */}
+      <nav className="md:hidden">
+        <div className="flex mt-2">
+          {buttonLabels.map((button, index) => (
+            <Link key={index} href={button.url ?? "#"}>
+              <button className="text-black hover:bg-slate-100 border-none py-1 px-2 rounded-sm">
+                {button.label}
+              </button>
+            </Link>
+          ))}
         </div>
       </nav>
     </header>
