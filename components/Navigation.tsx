7bc509a9b81df7d73111fbcc09655522ab1a1adb
jmao5 <jsh5890@kakao.com>
2024-02-27 16:24:07 +0900

    11

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
 import { Search } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function Navigation() {
   const buttonLabels = [
@@ -17,25 +16,30 @@ export default function Navigation() {
           <Link href={"/"}>로고</Link>
           <div className="pl-10 mt-2 md:mt-0 space-x-2">
             {buttonLabels.map((button, index) => (
-              <a key={index} href={button.url ?? "#"} className="inline-block">
-                <button className="text-black hover:bg-brand-primary-100 border-none py-1 px-2 rounded-sm">
+              <Link
+                key={index}
+                href={button.url ?? "#"}
+                className="inline-block"
+              >
+                <button className="text-black hover:bg-slate-100 border-none py-1 px-2 rounded-sm">
                   {button.label}
                 </button>
-              </a>
+              </Link>
             ))}
           </div>
         </div>
         <div className="flex items-center">
           <div className="flex items-center mt-2 md:mt-0 pr-3">
-            <div className="w-12 h-12 flex items-center justify-center"></div>
-            <Button variant="outlineNone" size="icon">
-              <Search />
-            </Button>
-            <input
-              type="text"
-              placeholder="제목을 검색해보세요"
-              className="border-black focus:border-main focus:outline-none placeholder-black placeholder-opacity-75 py-4"
-            />
+            <div className="flex items-center justify-center">
+              <Button variant="outlineNone" size="icon">
+                <Search />
+              </Button>
+              <input
+                type="text"
+                placeholder="제목을 검색해보세요"
+                className=" placeholder-black placeholder-opacity-75 py-2 pl-2"
+              />
+            </div>
           </div>
         </div>
       </nav>
