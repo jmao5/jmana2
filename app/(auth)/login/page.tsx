22983ecbe6deab4b3626eee2ee638c74a6dc3385
jmao5 <jsh5890@kakao.com>
2024-03-14 17:46:37 +0900

    ㄱㄱㄱ

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import LinkButton from "@/components/common/LinkButton";
-import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
@@ -13,14 +13,19 @@ const LoginPage = () => {
       <div className="flex flex-col items-center justify-center gap-20">
         {/* <h1 className={`${gugi.className} text-[3.4rem] font-black`}>manaJ</h1> */}
         <img src="/images/logo.png" alt="로고" width={300} height={300} />
-        <Link href={`${process.env.NEXT_PUBLIC_KAKAO_OAUTH_URL}`}>
+        <button
+          // href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.AUTH_URL}&response_type=code`}
+          onClick={() => {
+            router.push(`${process.env.NEXT_PUBLIC_KAKAO_OAUTH_URL}`);
+          }}
+        >
           <img
             src="/images/kakaoLogin 1.png"
             alt="카카오 로그인"
             width={300}
             height={60}
           />
-        </Link>
+        </button>
 
         {/* <Link href="/" className="mt-28 text-lg text-dark-gray underline">
           시작 페이지로 돌아가기
