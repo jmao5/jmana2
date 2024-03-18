d80ce63e11cdd78db8a61fd91eea1b0adc419ac7
jmao5 <jsh5890@kakao.com>
2024-03-19 00:52:31 +0900

    일단수정

--- components/domain/toon/ToonCard.tsx
+++ components/domain/toon/ToonCard.tsx
@@ -1,38 +1,49 @@
 "use client";
 
-import { ToonResponseList } from "@/type/response";
+import { ToonResponse, ToonResponseList } from "@/type/response";
 import Image from "next/image";
+import { useState } from "react";
 
 interface ToonCardProps {
   toonList: ToonResponseList;
   token?: string;
 }
-
 const ToonCard: React.FC<ToonCardProps> = ({ toonList, token }) => {
   console.log("화면단에서 토큰 : ", token);
 
   return (
     <>
       {token
-        ? toonList.map((item) => (
-            <li key={item.id}>
-              <a className="link block bg-white rounded-lg shadow-md overflow-hidden">
-                <div className="w-full h-32 relative">
-                  <Image src={item.imagePath} alt={item.title} layout="fill" />
-                </div>
-                <div className="p-2">
-                  <strong className="block text-md text-gray-900 truncate">
-                    {item.title}
-                  </strong>
-                  <span className="block text-gray-600 mt-1 text-sm truncate">
-                    {item.genre}
-                  </span>
-                </div>
-              </a>
-            </li>
-          ))
+        ? toonList.map((item) => <ToonItem key={item.id} item={item} />)
         : ""}
     </>
+  );
+};
+
+const ToonItem: React.FC<{ item: ToonResponse }> = ({ item }) => {
+  const [isImgError, setIsImgError] = useState<boolean>(false);
+
+  return (
+    <li>
+      <a className="link block bg-white rounded-lg shadow-md overflow-hidden">
+        <div className="w-full h-32 relative">
+          <Image
+            src={!isImgError ? item.imagePath : "/images/blur.jpg"}
+            alt={item.title}
+            layout="fill"
+            onError={() => setIsImgError(true)}
+          />
+        </div>
+        <div className="p-2">
+          <strong className="block text-md text-gray-900 truncate">
+            {item.title}
+          </strong>
+          <span className="block text-gray-600 mt-1 text-sm truncate">
+            {item.genre}
+          </span>
+        </div>
+      </a>
+    </li>
   );
 };
 
