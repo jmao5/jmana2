a94ba26caa71a7713d5e82a251e188287243cebe
jmao5 <jsh5890@kakao.com>
2024-03-18 17:45:33 +0900

    test

--- app/(navigation)/page.tsx
+++ app/(navigation)/page.tsx
@@ -1,26 +1,28 @@
 import { getToonList } from "@/apis/client/getToonList";
-import { getServerToken, getServerUserId } from "@/utils/auth";
+import { getServerToken } from "@/utils/auth";
 import Image from "next/image";
 
 export default async function Home() {
+  // const userId = getServerUserId();
   const token = getServerToken();
-  const userId = getServerUserId();
   const params = {
-    size: 10,
+    size: 100,
   };
   const { data: toonList } = await getToonList(params);
 
-  return (
-    <>
-      <div className="grid grid-cols-3 gap-4">
-        {toonList.map((item) => (
-          <div
-            className="shadow-md p-4 bg-white rounded-lg w-full h-32 relative #{!important}"
-            key={item.id}
-          >
+  return token ? (
+    <div className="grid grid-cols-3 gap-4">
+      {toonList.map((item) => (
+        <div
+          className="shadow-md p-4 bg-white rounded-lg w-full relative"
+          key={item.id}
+        >
+          <div className="h-28">
             <Image src={item.imagePath} alt={item.title} fill />
-            <div className="pt-2 h-20">
-              <div className="font-semibold text-lg leading-7">
+          </div>
+          <div className="h-4">
+            <div className="pt-2 h-8">
+              <div className="font-semibold text-xs leading-7">
                 <div className="flex items-center space-x-1.5">
                   <span className="truncate">{item.title}</span>
                 </div>
@@ -31,8 +33,10 @@ export default async function Home() {
               <div className="flex text-base leading-6 text-blackAlpha-500"></div>
             </div>
           </div>
-        ))}
-      </div>
-    </>
+        </div>
+      ))}
+    </div>
+  ) : (
+    ""
   );
 }
