6706e4d7fbced64826f41b385694dba84dd1633d
jmao5 <jsh5890@kakao.com>
2024-03-18 21:07:10 +0900

    css 수정

--- app/(navigation)/page.tsx
+++ app/(navigation)/page.tsx
@@ -10,33 +10,25 @@ export default async function Home() {
   };
   const { data: toonList } = await getToonList(params);
 
-  return token ? (
-    <div className="grid grid-cols-3 gap-4">
+  return (
+    <ul className="grid grid-cols-3 md:grid-cols-4 gap-4">
       {toonList.map((item) => (
-        <div
-          className="shadow-md p-4 bg-white rounded-lg w-full relative"
-          key={item.id}
-        >
-          <div className="h-28">
-            <Image src={item.imagePath} alt={item.title} fill />
-          </div>
-          <div className="h-4">
-            <div className="pt-2 h-8">
-              <div className="font-semibold text-xs leading-7">
-                <div className="flex items-center space-x-1.5">
-                  <span className="truncate">{item.title}</span>
-                </div>
-              </div>
-              <div className="text-cyan-600 font-semibold leading-6">
-                {item.genre}
-              </div>
-              <div className="flex text-base leading-6 text-blackAlpha-500"></div>
+        <li key={item.id}>
+          <a className="link block bg-white rounded-lg shadow-md overflow-hidden">
+            <div className="w-full h-32 relative">
+              <Image src={item.imagePath} alt={item.title} layout="fill" />
             </div>
-          </div>
-        </div>
+            <div className="p-4">
+              <strong className="block text-md font-semibold text-gray-800 truncate">
+                {item.title}
+              </strong>
+              <span className="block text-gray-600 mt-1 truncate text-xs">
+                {item.genre}
+              </span>
+            </div>
+          </a>
+        </li>
       ))}
-    </div>
-  ) : (
-    ""
+    </ul>
   );
 }
